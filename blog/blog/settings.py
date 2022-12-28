c91c46a486e1136c284395da62c632d9ccbfc986
Pav9551 <pav9551@yandex.ru>
2022-12-28 16:02:26 +0300

    added base.html

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [path.join(BASE_DIR, 'templates')],
+        'DIRS': [path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
