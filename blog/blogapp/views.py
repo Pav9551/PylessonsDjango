d0edec5ee8b9e01bcd0e5bcb1a10b5f5466e5c7f
Pav9551 <pav9551@yandex.ru>
2022-12-28 15:12:19 +0300

    added bootstrap

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Good
 # Create your views here.
+def main_view(request):
+    posts = Good.objects.all()
+    return render(request, 'blogapp/index.html')
