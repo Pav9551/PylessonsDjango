7a07eec43165bb207b4ca6bd8f2ede415f974498
Pav9551 <pav9551@yandex.ru>
2022-12-28 14:59:32 +0300

    added bootstrap

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [path.join(BASE_DIR,'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
---
+++ blog/blogapp/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+from blogapp import views
+app_name = 'blogapp'
+urlpatterns = [
+path('', views.main_view, name='index'),
+]
