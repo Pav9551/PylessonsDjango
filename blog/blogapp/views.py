dcc247227885a7f6488e19f33857244d2b6ad82d
Pav9551 <pav9551@yandex.ru>
2022-12-28 20:43:00 +0300

    added base

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -5,5 +5,5 @@ def main_view(request):
     good = Good.objects.all()
     return render(request, 'blogapp/index.html', context={'posts': good})
 def post(request, id):
-    post = get_object_or_404(Good, id=id)
-    return render(request, 'blogapp/post.html', context={'post': post})
+    good = get_object_or_404(Good, id=id)
+    return render(request, 'blogapp/post.html', context={'post': good})
