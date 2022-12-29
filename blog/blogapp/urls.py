59780dfe885acf9e1901eb2260ee0264aa76a264
Pav9551 <pav9551@yandex.ru>
2022-12-29 15:51:40 +0300

    added goods.html

--- blog/blogapp/urls.py
+++ blog/blogapp/urls.py
@@ -5,4 +5,5 @@ app_name = 'blogapp'
 urlpatterns = [
     path('', views.main_view, name='index'),
     path('merch/<int:id>/', views.post, name='merch'),
+    path('goods', views.goods, name='goods'),
 ]
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -7,3 +7,6 @@ def main_view(request):
 def post(request, id):
     merch = get_object_or_404(Merchandise, id=id)
     return render(request, 'blogapp/merch.html', context={'merch': merch})
+def goods(request):
+    goods = Good.objects.all()
+    return render(request, 'blogapp/goods.html', context={'goods': goods})
